3cd2286a1ce7a6ca1fe2948549c18469b464aa68
Sean Kilgarriff <seanpkilgarriff@gmail.com>
2016-04-06 17:00:25 -0400

    entrypoint script to install ghost

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,26 +1,11 @@
 #!/bin/sh
-set -e
+cd /usr/src/app
 
-if [[ "$*" == npm*start* ]]; then
-	for dir in "$GHOST_SOURCE/content"/*/; do
-		targetDir="$GHOST_CONTENT/$(basename "$dir")"
-		mkdir -p "$targetDir"
-		if [ -z "$(ls -A "$targetDir")" ]; then
-			tar -c --one-file-system -C "$dir" . | tar xC "$targetDir"
-		fi
-	done
+#Install all packages from Package.json.
+npm install
 
-	if [ ! -e "$GHOST_CONTENT/config.js" ]; then
-		sed -r '
-			s/127\.0\.0\.1/0.0.0.0/g;
-			s!path.join\(__dirname, (.)/content!path.join(process.env.GHOST_CONTENT, \1!g;
-		' "$GHOST_SOURCE/config.example.js" > "$GHOST_CONTENT/config.js"
-	fi
+#For updating
+#npm install -g npm-check-updates
+#ncu -d -u --packageData
 
-	ln -sf "$GHOST_CONTENT/config.js" "$GHOST_SOURCE/config.js"
-
-	chown -R ghost "$GHOST_CONTENT"
-
-fi
-
-exec "$@"
+npm start
