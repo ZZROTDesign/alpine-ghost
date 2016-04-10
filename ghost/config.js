15298d6edce60dbd1f149dd7dfe92b0fb83b70bc
Sean Kilgarriff <seanpkilgarriff@gmail.com>
2016-04-09 23:19:11 -0400

    added Environment variables into config.js

--- ghost/config.js
+++ ghost/config.js
@@ -11,7 +11,7 @@ config = {
     // When running Ghost in the wild, use the production environment.
     // Configure your URL and mail settings here
     production: {
-        url: 'http://my-ghost-blog.com',
+        url: process.env.PROD_DOMAIN,
         mail: {},
         database: {
             client: 'sqlite3',
@@ -31,7 +31,7 @@ config = {
     development: {
         // The url to use when providing links to the site, E.g. in RSS and email.
         // Change this to your Ghost blog's published URL.
-        url: 'http://localhost:2368',
+        url: process.env.DEV_DOMAIN,
 
         // Example mail config
         // Visit http://support.ghost.org/mail for instructions
