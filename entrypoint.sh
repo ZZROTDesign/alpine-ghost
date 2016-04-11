7582568e5039f160bb8c1b6874ddc59afacf1a14
Sean Kilgarriff <seanpkilgarriff@gmail.com>
2016-04-11 19:23:17 -0400

    added npm start

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,15 +1,20 @@
 #!/bin/sh
+
+#Set ENV variables
+set -e
+
+#Copy over $GHOST_SOURCE files to different directory
+baseDir="$GHOST_SOURCE/content"
+for dir in "$baseDir"/*/ "$baseDir"/themes/*/; do
+	targetDir="$GHOST_CONTENT/${dir#$baseDir/}"
+	mkdir -p "$targetDir"
+	if [ -z "$(ls -A "$targetDir")" ]; then
+		tar -c --one-file-system -C "$dir" . | tar xC "$targetDir"
+	fi
+done
+
+#Move into the Ghost directory
 cd /usr/src/app
 
-	baseDir="$GHOST_SOURCE/content"
-	for dir in "$baseDir"/*/ "$baseDir"/themes/*/; do
-		targetDir="$GHOST_CONTENT/${dir#$baseDir/}"
-		mkdir -p "$targetDir"
-		if [ -z "$(ls -A "$targetDir")" ]; then
-			tar -c --one-file-system -C "$dir" . | tar xC "$targetDir"
-		fi
-	done
-
-	npm install
-
-	npm start
+#Start Ghost
+npm start
