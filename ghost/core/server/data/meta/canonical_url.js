59088f3c9d8ebc28f39a5d4ceb65b6850a0585e8
Sean Kilgarriff <seanpkilgarriff@gmail.com>
2016-04-11 18:44:45 -0400

    removed ghost file for different dockerfile structure

--- ghost/core/server/data/meta/canonical_url.js
+++
@@ -1,9 +0,0 @@
-var config = require('../../config'),
-    getUrl = require('./url');
-
-function getCanonicalUrl(data) {
-    return config.urlJoin(config.getBaseUrl(false),
-        getUrl(data, false));
-}
-
-module.exports = getCanonicalUrl;
